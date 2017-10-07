d646162272b7663db36091288a304ef9f60ef91e
Jerry Junhao Ying <enjoysoft@hotmail.com>
2017-10-07 21:36:34 +0800

    Better version

--- Scala/src/com/enjoysoft/scala/Food.scala
+++ Scala/src/com/enjoysoft/scala/Food.scala
@@ -5,11 +5,13 @@ import java.time.temporal.ChronoUnit
 import java.time.{LocalDate, Period}
 
 
-case class Food(name: String, meat: Boolean, add: Boolean)
+final case class Food(name: String, meat: Boolean, add: Boolean) {
+  override def toString: String = name
+}
 
 object Food {
-  val Meat = '*'
-  val Add = '+'
+  private val Meat = '*'
+  private val Add = '+'
 
   def apply(s: String): Food = Food(name(s), meat(s), add(s))
 
@@ -25,51 +27,52 @@ object Food {
 
 object Main extends App {
 
-  val testTxtSource = scala.io.Source.fromFile("C:\\Users\\enjoy\\Desktop\\food.txt")
+  val lines = scala.io.Source.fromFile("C:\\Users\\enjoy\\Desktop\\food.txt").getLines()
 
-  val lines = testTxtSource.getLines()
+  val foods = (for (line <- lines) yield Food(line.trim())).toList
 
-  val foods: List[Food] = (for (line <- lines) yield Food(line.trim())).toList
-  val Days = 100
-  val Week = 6
-  val Birthday = LocalDate.of(2017, 2, 16)
   var (newFoods, oldFoods) = foods.partition(_.add)
-  var food1: Food = oldFoods.filterNot(_.meat).head
-  var food2: Food = pickVeg(oldFoods, food1)
-  var food3: Option[Food] = None
-  var meatFoods: List[Food] = oldFoods.filter(_.meat)
-  var meatQueue = Iterator.continually(meatFoods).flatten
 
-  def pickVeg(foods: List[Food], foodEx: Food): Food = {
-    val veg = foods.filterNot(_.meat)
-    pickFood(veg, foodEx)
+  var meats = oldFoods.filter(_.meat)
+  var vegetables1 = oldFoods.filterNot(_.meat)
+  var vegetables2 = rotate(vegetables1)
+
+  val Days = 200
+  val Week = 7
+  val Birthday = LocalDate.of(2017, 2, 18)
+
+  def rotate(list: List[Food]): List[Food] = {
+    val head :: tail = list
+    if (head == tail.head) tail else tail :+ head
   }
 
-  def pickFood(foods: List[Food], foodEx: Food): Food = scala.util.Random.shuffle(foods.filterNot(_ == foodEx)).head
-
   val outputs = for (i <- 1 to Days) yield {
+
     val date = LocalDate.now().plusDays(i)
     val period = Period.between(Birthday, date)
+    val months = period.getMonths
     val days = period.getDays
     val d = ChronoUnit.DAYS.between(Birthday, date)
-    val months = period.getMonths
+
     if (i % 3 == 1 && newFoods.nonEmpty) {
       // add new food every three days
       val newFood = newFoods.head
-      oldFoods = oldFoods :+ newFood
-      newFoods = newFoods.drop(1)
-      meatFoods = if (newFood.meat) meatFoods :+ newFood else meatFoods
-      food2 = food1
-      food1 = if (newFood.meat) pickVeg(oldFoods, food2) else newFood
-      food3 = if (newFood.meat) Some(newFood) else None
-    } else {
-      food2 = food1
-      food1 = pickVeg(oldFoods, food2)
-      food3 = if (i % Math.max(Week / meatFoods.size, 2) == 0) {
-        Some(meatQueue.next)
-      } else None
+      newFoods = newFoods.tail
+      if (newFood.meat) meats = newFood :: newFood :: newFood :: meats
+      else {
+        vegetables1 = newFood :: newFood :: newFood :: vegetables1
+        vegetables2 = vegetables2.init :+ newFood :+ vegetables2.last
+      }
     }
-    val foo = s"$date,${months}月${days}天,$d,${food1.name},${food2.name},${food3.map(_.name).getOrElse("")}"
+
+    val veg1 = vegetables1.head
+    vegetables1 = rotate(vegetables1)
+    val veg2 = vegetables2.head
+    vegetables2 = rotate(vegetables2)
+    val meat = meats.head
+    meats = rotate(meats)
+
+    val foo = s"$date,${months}月${days}天,$d,${veg1.name},${veg2.name},${meat.name}"
     println(foo)
     foo
   }
@@ -79,5 +82,5 @@ object Main extends App {
   import scala.collection.JavaConverters._
 
   Files.write(Paths.get("C:\\Users\\enjoy\\Desktop\\food.csv"), ("日期,月龄,天数,辅食1,辅食2,辅食3" +: outputs).asJava,
-    Charset.forName("GBK"))
+    Charset.forName("GB18030"))
 }
